7c399ac68610bd0ac25c1d55755a98086085d62c
everton <everton.nrb@hotmail.com>
2016-09-30 06:53:38 -0400

    Telas

---
+++ Apresentacao/FrmMenu.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Apresentacao
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labelVersao = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.menuCadastro = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCliente = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelVersao});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "BarraDeStatusPrincipal";
+            // 
+            // labelVersao
+            // 
+            this.labelVersao.Name = "labelVersao";
+            this.labelVersao.Size = new System.Drawing.Size(59, 17);
+            this.labelVersao.Text = "Versão 1.0";
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCadastro,
+            this.menuSair});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(784, 24);
+            this.menuPrincipal.TabIndex = 2;
+            this.menuPrincipal.Text = "menuStrip1";
+            // 
+            // menuCadastro
+            // 
+            this.menuCadastro.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCliente});
+            this.menuCadastro.Name = "menuCadastro";
+            this.menuCadastro.Size = new System.Drawing.Size(66, 20);
+            this.menuCadastro.Text = "Cadastro";
+            this.menuCadastro.Click += new System.EventHandler(this.cadastroToolStripMenuItem_Click);
+            // 
+            // menuCliente
+            // 
+            this.menuCliente.Name = "menuCliente";
+            this.menuCliente.Size = new System.Drawing.Size(152, 22);
+            this.menuCliente.Text = "Cliente";
+            // 
+            // menuSair
+            // 
+            this.menuSair.Name = "menuSair";
+            this.menuSair.Size = new System.Drawing.Size(38, 20);
+            this.menuSair.Text = "Sair";
+            this.menuSair.Click += new System.EventHandler(this.menuSair_Click);
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuPrincipal);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuPrincipal;
+            this.Name = "FrmMenu";
+            this.Text = "Menu principal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labelVersao;
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem menuCadastro;
+        private System.Windows.Forms.ToolStripMenuItem menuCliente;
+        private System.Windows.Forms.ToolStripMenuItem menuSair;
+    }
+}
